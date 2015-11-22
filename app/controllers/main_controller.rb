7704eb380c28f00d6c47bc8e07d276e835cf3048
Lyudmil Lesinski <lyudmilles@gmail.com>
2015-11-22 17:36:26 +0100

    Updated table view of content_groups#index and root page
    Now the table shows the last contest added to the group, start and end
    time and duration of that contest

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,6 +6,7 @@ class MainController < ApplicationController
   before_filter :check_user_profile
 
   def index
+    @contest_groups = ContestGroup.paginate(:page => params[:page], :per_page => 20)
     @past_contests = Contest.finished.paginate(:page => params.fetch(:past_contest_page, 1), :per_page => 20)
     @contests = WillPaginate::Collection.create(params[:contest_page] || 1, 20) do |pager|
       contests = Contest.current.select {|contest| contest.visible or current_user.andand.admin?}
